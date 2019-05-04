8b4240eb2ccfbc9e4d4e95d65a8395cdb66ca20c
sezer.halici <sehalici@outlook.com>
2019-05-04 14:01:10 +0300

    jsf

--- src/main/java/services/PersonelService.java
+++ src/main/java/services/PersonelService.java
@@ -6,9 +6,31 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class PersonelServices implements IPersonelservices{
+public class PersonelService implements IPersonelservices{
 
     private List<Personel> personelList=new ArrayList<Personel>();
+    private Long idGenerator=1L;
+    private static PersonelService instance;
+
+
+
+    /**
+     * PerseonelService nesnesinin birkere new anahtar sözcüğü ile oluşturulması için
+     * Singleton Design patter uygulayacak
+     * Nesnenin birkere oluşmasından sorumlu tasarım desenidir.
+     *
+     */
+
+    public static PersonelService getInstance() {
+       if(instance==null){
+           instance=new PersonelService();
+       }
+       return instance;
+    }
+
+
+
+
 
     public void kaydet(Personel personel) {
 
@@ -16,7 +38,7 @@ public class PersonelServices implements IPersonelservices{
 
             Personel yeniPersonel=new Personel();
 
-            yeniPersonel.setId(personel.getId());
+            yeniPersonel.setId(idGenerator);
             yeniPersonel.setDepartman(personel.getDepartman());
             yeniPersonel.setIseGirisTarih(personel.getIseGirisTarih());
             yeniPersonel.setMaas(personel.getMaas());
@@ -30,6 +52,8 @@ public class PersonelServices implements IPersonelservices{
             yeniPersonel.setDogumTarihi(personel.getDogumTarihi());
             yeniPersonel.setTcNo(personel.getTcNo());
             yeniPersonel.setTelNo(personel.getTelNo());
+
+            idGenerator++;
 
             personelList.add(yeniPersonel);
 
--- src/main/java/view/PersonelView.java
+++ src/main/java/view/PersonelView.java
@@ -1,7 +1,82 @@
 package view;
 
+import entity.Personel;
+import services.PersonelService;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
 public class PersonelView {
 
+    private Personel personel;
+    private PersonelService personelServices;
+    private List<Personel> personelList;
+    private List<String> departmanList;
 
+    @PostConstruct
+    public void init(){
+     personel= new Personel();
+     personelServices=new PersonelService();
+     personelList = new ArrayList<>();
+     departmanList = new ArrayList<>();
 
+     departmanList.add("Arge");
+     departmanList.add("İnsan Kaynakları");
+     departmanList.add("Yönetim");
+     departmanList.add("Mali İşler");
+     departmanList.add("Hizmetli");
+
+    }
+
+    /**
+     * Kaydet butonuna tıklandığında çağırılacak method
+     * ActionListener ile asyn (asenkron) olarak çağırılıyor.
+     */
+    public void onKaydet() {
+
+        if (personel.getAd()!=null){
+            personelServices.kaydet(personel);
+            personelList=personelServices.personelListesiGetir();
+            FacesContext.getCurrentInstance().addMessage(null,new FacesMessage("Kayıt işlemi başarılı..",null));
+        }else {
+            FacesContext.getCurrentInstance().addMessage(null,new FacesMessage("Kayıt işlemi başarısız..",null));
+        }
+    }
+
+    public Personel getPersonel() {
+        return personel;
+    }
+
+    public void setPersonel(Personel personel) {
+        this.personel = personel;
+    }
+
+    public List<Personel> getPersonelList() {
+        return personelList;
+    }
+
+    public void setPersonelList(List<Personel> personelList) {
+        this.personelList = personelList;
+    }
+
+    public PersonelService getPersonelServices() {
+        return personelServices;
+    }
+
+    public void setPersonelServices(PersonelService personelServices) {
+        this.personelServices = personelServices;
+    }
+
+    public List<String> getDepartmanList() {
+        return departmanList;
+    }
+
+    public void setDepartmanList(List<String> departmanList) {
+        this.departmanList = departmanList;
+    }
 }
